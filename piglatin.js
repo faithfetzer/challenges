4e58669d0891d66bae91c1cfbe1011b00788678a
Faith Fetzer <faithfetzer@Faiths-Air.attlocal.net>
2021-05-10 22:06:52 -0400

    replit 3, 4, piglatin

---
+++ piglatin.js
@@ -0,0 +1,44 @@
+/*Pig Latin Challenge
+Create a function that takes in strings
+In the function, translate a phrase into pig latin and print it to the console.
+If able to do so, return the value into another variable and print that variable.
+What is Pig Latin?
+If the word begins with a consonant, remove the consonant to the first vowel, place it at the end of the word, and add an 'ay' to the end (i.e. Pig Latin => IgPay Atinlay).
+If the word begins with a vowel, simply add an 'ay' at the end of the word.
+*/
+// string>array
+// if vowel then
+function pigLatin(string) {
+    let wordArray = string.split (' ');
+    let newWordArray= [];
+    wordArray.forEach(element => {    
+        const stringArray = element.split('');
+    // let stringArray = string.split('');
+    // console.log(stringArray);
+        const firstCharacter = stringArray[0];
+    // console.log(firstCharacter);
+        if(firstCharacter !== "a"  && firstCharacter !== "e" && firstCharacter !== "i" && firstCharacter !== "o" && firstCharacter !== "u" && firstCharacter !== "y"){
+            //starts with consonants
+            // console.log('consonant');
+            stringArray.push(firstCharacter);
+            // console.log(stringArray);
+            stringArray.shift();
+            // console.log(stringArray);
+        }
+        stringArray.push('ay'); //add ay in all of the words- wther vowel or consonant
+        // stringArray.join('');//bring array back to a string
+        // console.log(stringArray.join(''));
+        //return the string
+        newWordArray.push(stringArray.join(''));
+    });
+    console.log(newWordArray.join(' '));
+}
+pigLatin('aye');
+pigLatin('hello');
+pigLatin('eye');
+pigLatin('index');
+pigLatin('option');
+pigLatin('under');
+pigLatin('you');
+pigLatin('Star Wars');
+pigLatin('Hello how are you')
